95467b56c66007c53a737671124ff8064795fe7d
joao-o-frade <joao.o.frade@gmail.com>
2017-08-10 13:53:14 -0300

    ed
    ajuste no component SelectionGridMult, para receber lista de projetos

--- src/module/project/vuex/getters.js
+++ src/module/project/vuex/getters.js
@@ -8,11 +8,11 @@ export const projectsLoading = ({ projectsLoading }) => projectsLoading
 
 export const selectedProjects = ({ selectedProjects }) => selectedProjects
 
-export const projectsFilteredByTerm = ({ projectFilterProperties, projectFilterTerm }) => projectsFiltered => {
+export const projectsFilteredByTerm = ({ projectFilterProperties, projectFilterTerm }) => projects => {
   if (projectFilterTerm !== '') {
     let words = projectFilterTerm.split('+')
 
-    return projectsFiltered.filter(item => {
+    return projects.filter(item => {
       return words.every(word => {
         return projectFilterProperties.some(filterProperty => {
           return item[filterProperty.name].toLowerCase().indexOf(word.toLowerCase()) > -1
@@ -20,7 +20,7 @@ export const projectsFilteredByTerm = ({ projectFilterProperties, projectFilterT
       })
     })
   } else {
-    return projectsFiltered
+    return projects
   }
 }
 
